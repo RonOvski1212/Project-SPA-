ad2ca7a63868ed95315ae8dac500a98e21dd2917
RonOvski1212 <245785gio@gmail.com>
2023-11-19 00:13:08 +0400

    finish

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const numberOfSlides = slides.length;
+let slideNumber = 0;
 
+nextBtn.onclick = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
 
-//slider next button
+  slideNumber++;
 
+  if (slideNumber > numberOfSlides - 1) {
+    slideNumber = 0;
+  }
 
-//slider prev button
+  slides[slideNumber].classList.add("active");
+};
+
+prevBtn.onclick = () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  slideNumber--;
+
+  if (slideNumber < 0) {
+    slideNumber = numberOfSlides - 1;
+  }
+
+  slides[slideNumber].classList.add("active");
+};
